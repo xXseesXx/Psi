45204a36cc3a994869e26dfec44832ad944922c9
xXseesXx <fabibeer2004@gmail.com>
2026-08-24 17:09:35 +0200

    Fix: Simplify executeExplodeTest to directly use player position instead of entity selector chain

--- src/main/java/vazkii/psi/common/command/CommandPsiTest.java
+++ src/main/java/vazkii/psi/common/command/CommandPsiTest.java
@@ -20,8 +20,6 @@ import vazkii.psi.api.spell.Spell;
 import vazkii.psi.api.spell.SpellContext;
 import vazkii.psi.api.spell.SpellRuntimeException;
 import vazkii.psi.common.spell.operator.PieceOperatorSum;
-import vazkii.psi.common.spell.selector.PieceSelectorCaster;
-import vazkii.psi.common.spell.selector.PieceSelectorEntityPosition;
 import vazkii.psi.common.spell.selector.PieceSelectorRaycast;
 import vazkii.psi.common.spell.trick.PieceTrickBreakBlock;
 import vazkii.psi.common.spell.trick.PieceTrickDebug;
@@ -345,32 +343,8 @@ public class CommandPsiTest extends CommandBase {
         SpellContext context = new SpellContext().setPlayer(player);
 
         try {
-            // Get player's position using entity position selector
-            // Chain: PieceSelectorCaster -> Entity -> PieceSelectorEntityPosition -> Vector3
-
-            // First get the caster entity
-            PieceSelectorCaster casterSelector = new PieceSelectorCaster(spell) {
-
-                @Override
-                public Object execute(SpellContext ctx) {
-                    return ctx.caster;
-                }
-            };
-            final Object casterEntity = casterSelector.execute(context);
-
-            // Convert entity to position Vector3
-            PieceSelectorEntityPosition posSelector = new PieceSelectorEntityPosition(spell) {
-
-                @Override
-                public <T> T getParamValue(SpellContext ctx, vazkii.psi.api.spell.SpellParam<T> param) {
-                    if (param == this.target) {
-                        return (T) casterEntity;
-                    }
-                    return null;
-                }
-            };
-            posSelector.initParams();
-            final Object position = posSelector.execute(context);
+            // Get player's position directly - simpler than using entity selector chain
+            final Vector3 playerPosition = new Vector3(player.posX, player.posY, player.posZ);
 
             // Create explosion at player position
             final double explosionPower = 3.0; // TNT is 4.0, this is slightly weaker
@@ -380,7 +354,7 @@ public class CommandPsiTest extends CommandBase {
                 @Override
                 public <T> T getParamValue(SpellContext ctx, vazkii.psi.api.spell.SpellParam<T> param) {
                     if (param == this.position) {
-                        return (T) position;
+                        return (T) playerPosition;
                     }
                     if (param == this.power) {
                         return (T) (Double) explosionPower;
@@ -393,13 +367,16 @@ public class CommandPsiTest extends CommandBase {
             explodeTrick.execute(context);
 
             // Success message
-            Vector3 pos = (Vector3) position;
             sender.addChatMessage(
                 new ChatComponentText(EnumChatFormatting.GREEN + "[Psi] Explode spell executed successfully"));
             sender.addChatMessage(
                 new ChatComponentText(
                     EnumChatFormatting.GRAY + "Explosion position: "
-                        + String.format("Vector3{x=%.1f, y=%.1f, z=%.1f}", pos.x, pos.y, pos.z)));
+                        + String.format(
+                            "Vector3{x=%.1f, y=%.1f, z=%.1f}",
+                            playerPosition.x,
+                            playerPosition.y,
+                            playerPosition.z)));
             sender.addChatMessage(new ChatComponentText(EnumChatFormatting.GRAY + "Power: " + explosionPower));
 
         } catch (SpellRuntimeException e) {
